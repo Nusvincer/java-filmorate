619c4cc5442dd861cc48ef285aa2445932438dd3
yandere <nusvincer@gmail.com>
2025-01-02 03:40:13 +0400

    fix Checkstyle autotest 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     Film getFilm(int id);
+
     void deleteFilm(int id);
+
     List<Film> getAllFilms();
 }
